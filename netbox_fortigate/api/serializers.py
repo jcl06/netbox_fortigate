0eca198ffa01858e8c903dfa4a298e9894830e5d
Joel Loreno <jcloreno@gmail.com>
2026-03-05 14:42:09 -0800

    Update

--- netbox_fortigate/api/serializers.py
+++ netbox_fortigate/api/serializers.py
@@ -1,47 +1,118 @@
-from netbox.api.serializers import NetBoxModelSerializer
+from rest_framework import serializers
 
 from ..models import *
 
 
-class FortigateSerializer(NetBoxModelSerializer):
+
+class _BaseEventSerializer(serializers.ModelSerializer):
+    """
+    Minimal serializer used for NetBox event/change serialization.
+
+    IMPORTANT:
+    - Use DRF ModelSerializer (NOT NetBoxModelSerializer) to avoid requiring API routes
+      for hyperlinked `url` fields.
+    """
     class Meta:
+        fields = "__all__"
+
+
+class FortigateSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = Fortigate
-        fields = "__all__"
 
 
 
-class InterfacesSerializer(NetBoxModelSerializer):
-    class Meta:
+class InterfacesSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = Interfaces
-        fields = "__all__"
 
 
-class ZoneSerializer(NetBoxModelSerializer):
-    class Meta:
+class ZoneSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = Zone
-        fields = "__all__"
 
 
-class RoutingTableSerializer(NetBoxModelSerializer):
-    class Meta:
+class RoutingTableSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = RoutingTable
-        fields = "__all__"
 
 
-class ObjectSerializer(NetBoxModelSerializer):
-    class Meta:
+class ObjectSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = Object
-        fields = "__all__"
 
 
-class SchedulerSerializer(NetBoxModelSerializer):
-    class Meta:
+class SchedulerSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = Scheduler
-        fields = "__all__"
 
 
 
-class PolicySerializer(NetBoxModelSerializer):
-    class Meta:
+class AddressSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = Address
+
+
+class AddressGroupSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = AddressGroup
+
+
+class ServicesSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = Services
+
+
+class ServiceGroupSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = ServiceGroup
+
+
+class VIPSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = VIP
+
+
+class VIPGroupSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = VIPGroup
+
+
+class ScheduleOnetimeSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = ScheduleOnetime
+
+
+class ScheduleRecurringSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = ScheduleRecurring
+
+
+class ScheduleGroupSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = ScheduleGroup
+
+
+class UserSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = User
+
+
+class AuthenticationServerSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = AuthenticationServer
+
+
+class PolicySerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
         model = Policy
-        fields = "__all__"
+
+
+class ProfileGroupSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = ProfileGroup
+
+
+class UserGroupSerializer(_BaseEventSerializer):
+    class Meta(_BaseEventSerializer.Meta):
+        model = UserGroup
